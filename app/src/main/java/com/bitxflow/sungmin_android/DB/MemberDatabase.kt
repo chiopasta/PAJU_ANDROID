d19db71080feb2af52cbb8acb9454149767e5281
bitxflow <bitxflow@gmail.com>
2020-02-09 00:37:51 +0900

    .version check

--- app/src/main/java/com/bitxflow/sungmin_android/DB/MemberDatabase.kt
+++ app/src/main/java/com/bitxflow/sungmin_android/DB/MemberDatabase.kt
@@ -5,8 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-//TODO version1 로 맞출것
-@Database(entities = arrayOf(User::class),version = 2)
+@Database(entities = arrayOf(User::class),version = 1)
 abstract class MemberDatabase : RoomDatabase()
 {
     abstract fun userDao():UserDao
